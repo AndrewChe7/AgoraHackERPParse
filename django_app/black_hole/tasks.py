59ce212bc54d6336df4df584033e00a697371007
azrail <zuic2272@yandex.ru>
2022-08-21 12:41:11 +0300

    hh

--- django_app/black_hole/tasks.py
+++ django_app/black_hole/tasks.py
@@ -1,3 +1,11 @@
 from celery import shared_task
-from agregator import models
-from agregator import serializers
+
+
+from django.apps import apps
+
+@shared_task
+def crate_product(json_payload):
+    model = apps.get_model('agregator', 'Product')
+    model = apps.get_model('agregator', 'MeasureUnit')
+    model = apps.get_model('agregator', 'Category')
+    pass
